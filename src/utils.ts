8ba0a87a6c5c94dfd8b97ac83276680cb5c9ec1f
eskander-taher <eskander.alshaibany@gmail.com>
2025-02-06 01:54:27 +0300

    sort by date

--- src/utils.ts
+++ src/utils.ts
@@ -28,7 +28,7 @@ export function formatDate(isoDate: string | undefined): string {
 	if(!isoDate) return ""
 
 	const date = new Date(isoDate);
-	const options: Intl.DateTimeFormatOptions = { year: "numeric", month: "long", day: "numeric" };
+	const options: Intl.DateTimeFormatOptions = { year: "numeric", month: "numeric", day: "numeric" };
 	return date.toLocaleDateString(undefined, options);
 }
 
