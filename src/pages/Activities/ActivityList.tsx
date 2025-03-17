4e5862a2c42343cee17066150a80ef91ded5066f
a-gaoba <abdulrahman1997abdo@gmail.com>
2025-03-17 20:36:57 +0300

    fix the activities page

--- src/pages/Activities/ActivityList.tsx
+++ src/pages/Activities/ActivityList.tsx
@@ -41,7 +41,7 @@ const ActivityList: React.FC = () => {
   };
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {data &&
         sortByDate(data).map((activity: Activity) => (
           <Card
